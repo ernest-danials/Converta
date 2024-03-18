c44064fe71ade48bc01f97de46a3d1cf493cf41b
Ernest Danials <97393666+ernest-danials@users.noreply.github.com>
2024-03-18 14:56:22 +0900

    Others

---
+++ Converta/Model/Trip.swift
@@ -0,0 +1,8 @@
+//
+//  Trip.swift
+//  Converta
+//
+//  Created by Ernest Dainals on 08/08/2023.
+//
+
+import Foundation
---
+++ Converta/Views/Screens/TripView.swift
@@ -0,0 +1,18 @@
+//
+//  TripView.swift
+//  Converta
+//
+//  Created by Ernest Dainals on 08/08/2023.
+//
+
+import SwiftUI
+
+struct TripView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TripView()
+}
